38d4603fae3b309440ae9ab1b215a5f6ca947192
Nyukeit <nyukeitthakkar@gmail.com>
2020-08-06 14:32:31 +0530

    Avatar Import

--- src/util/randomAvatarGen.js
+++ src/util/randomAvatarGen.js
@@ -1,11 +1,252 @@
 import svg1 from '../components/static/avatars/1-panickypanda.svg';
-
-
+import svg2 from '../components/static/avatars/2-lawlesslion.svg';
+import svg3 from '../components/static/avatars/3-terrifictiger.svg';
+import svg4 from '../components/static/avatars/4-bafflingbear.svg';
+import svg5 from '../components/static/avatars/5-paltryparrot.svg';
+import svg6 from '../components/static/avatars/6-radicalrabbit.svg';
+import svg7 from '../components/static/avatars/7-callouschameleon.svg';
+import svg8 from '../components/static/avatars/8-slowsloth.svg';
+import svg9 from '../components/static/avatars/9-eccentricelk.svg';
+import svg10 from '../components/static/avatars/10-lamellama.svg';
+import svg11 from '../components/static/avatars/11-ableanteater.svg';
+import svg12 from '../components/static/avatars/12-edgyeagle.svg';
+import svg13 from '../components/static/avatars/13-crispycrocodile.svg';
+import svg14 from '../components/static/avatars/14-ballisticbeaver.svg';
+import svg15 from '../components/static/avatars/15-hastyhamster.svg';
+import svg16 from '../components/static/avatars/16-wittywalrus.svg';
+import svg17 from '../components/static/avatars/17-beardedbear.svg';
+import svg18 from '../components/static/avatars/18-cosmiccheetah.svg';
+import svg19 from '../components/static/avatars/19-keenkangaroo.svg';
+import svg20 from '../components/static/avatars/20-deadlyduck.svg';
+import svg21 from '../components/static/avatars/21-gaudygoose.svg';
+import svg22 from '../components/static/avatars/22-leisurelylemur.svg';
+import svg23 from '../components/static/avatars/23-oddostrich.svg';
+import svg24 from '../components/static/avatars/24-ominousowl.svg';
+import svg25 from '../components/static/avatars/25-bipolarbear.svg';
+import svg26 from '../components/static/avatars/26-pickypenguin.svg';
+import svg27 from '../components/static/avatars/27-cageycamel.svg';
+import svg28 from '../components/static/avatars/28-rationalraccoon.svg';
+import svg29 from '../components/static/avatars/29-heavyhippo.svg';
+import svg30 from '../components/static/avatars/30-manicmonkey.svg';
+import svg31 from '../components/static/avatars/31-martialmeerkat.svg';
+import svg32 from '../components/static/avatars/32-slitheringsnake.svg';
+import svg33 from '../components/static/avatars/33-zestyzebra.svg';
+import svg34 from '../components/static/avatars/34-defunctdonkey.svg';
+import svg35 from '../components/static/avatars/35-blindbull.svg';
+import svg36 from '../components/static/avatars/36-gigglygoat.svg';
+import svg37 from '../components/static/avatars/37-giftedgoat.svg';
+import svg38 from '../components/static/avatars/38-handyhorse.svg';
+import svg39 from '../components/static/avatars/39-wackywolf.svg';
+import svg40 from '../components/static/avatars/40-kindkoala.svg';
+import svg41 from '../components/static/avatars/41-highhedgehog.svg';
+import svg42 from '../components/static/avatars/42-futilefrog.svg';
+import svg43 from '../components/static/avatars/43-timelessturtle.svg';
+import svg44 from '../components/static/avatars/44-gentlegorilla.svg';
+import svg45 from '../components/static/avatars/45-giantgiraffe.svg';
+import svg46 from '../components/static/avatars/46-dashingdeer.svg';
+import svg47 from '../components/static/avatars/47-rowdyrhino.svg';
+import svg48 from '../components/static/avatars/48-eliteelephant.svg';
+import svg49 from '../components/static/avatars/49-paceypuma.svg';
+import svg50 from '../components/static/avatars/50-finickyfox.svg';
 
 let avatars = [{
   path:svg1,
-  name:"panickypanda"
+  name:"Panicky Panda"
 },{
-  path:svg1,
-  name:"panickypanda"
+  path:svg2,
+  name:"Lawless Lion"
+},
+{
+  path:svg3,
+  name:"Terrific Tiger"
+},
+{
+  path:svg4,
+  name:"Baffling Bear"
+},
+{
+  path:svg5,
+  name:"Paltry Parrot"
+},
+{
+  path:svg6,
+  name:"Radical Rabbit"
+},
+{
+  path:svg7,
+  name:"Callous Chameleon"
+},
+{
+  path:svg8,
+  name:"Slow Sloth"
+},
+{
+  path:svg9,
+  name:"Eccentric Elk"
+},
+{
+  path:svg10,
+  name:"Lame Llama"
+},
+{
+  path:svg11,
+  name:"Able Anteater"
+},
+{
+  path:svg12,
+  name:"Edgy Eagle"
+},
+{
+  path:svg13,
+  name:"Crispy Crocodile"
+},
+{
+  path:svg14,
+  name:"Ballistic Beaver"
+},
+{
+  path:svg15,
+  name:"Hasty Hamster"
+},
+{
+  path:svg16,
+  name:"Witty Walrus"
+},
+{
+  path:svg17,
+  name:"Bearded Bear"
+},
+{
+  path:svg18,
+  name:"Cosmic Cheetah"
+},
+{
+  path:svg19,
+  name:"Keen Kangaroo"
+},
+{
+  path:svg20,
+  name:"Deadly Duck"
+},
+{
+  path:svg21,
+  name:"Gaudy Goose"
+},
+{
+  path:svg22,
+  name:"Leisurely Lemur"
+},
+{
+  path:svg23,
+  name:"Odd Ostrich"
+},
+{
+  path:svg24,
+  name:"Ominous Owl"
+},
+{
+  path:svg25,
+  name:"Bipolar Bear"
+},
+{
+  path:svg26,
+  name:"Picky Penguin"
+},
+{
+  path:svg27,
+  name:"Cagey Camel"
+},
+{
+  path:svg28,
+  name:"Rational Raccoon"
+},
+{
+  path:svg29,
+  name:"Heavy Hippo"
+},
+{
+  path:svg30,
+  name:"Manic Monkey"
+},
+{
+  path:svg31,
+  name:"Martial Meerkat"
+},
+{
+  path:svg32,
+  name:"Slithering Snake"
+},
+{
+  path:svg33,
+  name:"Zesty Zebra"
+},
+{
+  path:svg34,
+  name:"Defunct Donkey"
+},
+{
+  path:svg35,
+  name:"Blind Bull"
+},
+{
+  path:svg36,
+  name:"Giggly Goat"
+},
+{
+  path:svg37,
+  name:"Gifted Goat"
+},
+{
+  path:svg38,
+  name:"Handy Horse"
+},
+{
+  path:svg39,
+  name:"Wacky Wolf"
+},
+{
+  path:svg40,
+  name:"Kind Koala"
+},
+{
+  path:svg41,
+  name:"High Hedgehog"
+},
+{
+  path:svg42,
+  name:"Futile Frog"
+},
+{
+  path:svg43,
+  name:"Timless Turtle"
+},
+{
+  path:svg44,
+  name:"Gentle Gorilla"
+},
+{
+  path:svg45,
+  name:"Giant Giraffe"
+},
+{
+  path:svg46,
+  name:"Dashing Deer"
+},
+{
+  path:svg47,
+  name:"Rowdy Rhino"
+},
+{
+  path:svg48,
+  name:"Elite Elephant"
+},
+{
+  path:svg49,
+  name:"Pacey Puma"
+},
+{
+  path:svg50,
+  name:"Finicky Fox"
 }]
+
+
