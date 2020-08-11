e3491e7441d1bf617b8d070dd519c70dc54f9b2c
javkarprajeesh <prajeesh@iaspire.tech>
2020-08-11 13:49:06 +0530

    footer changes

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style.css';
+import Logo from '../static/Logo.svg'
 
 const Footer = () => {
     return (
         <div>
             <div className = "footerContainer">
-                <p className = "logo">© Vegh</p>
+                <img className = "logo" src = {Logo} alt = "Vegh Logo"/>
                 <div className = "linkContainer">
                 <p className = "about"><a href = "www.google.com">Terms of Service</a><span> |</span></p>
                 <p className = "tos"><a href = "www.google.com">About</a><span> |</span></p>
