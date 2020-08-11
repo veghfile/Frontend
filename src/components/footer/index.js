07be469e3dc9dedcc13e1010e4a48ccf37df0c78
javkarprajeesh <prajeesh@iaspire.tech>
2020-08-11 14:05:20 +0530

    footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -8,8 +8,8 @@ const Footer = () => {
             <div className = "footerContainer">
                 <img className = "logo" src = {Logo} alt = "Vegh Logo"/>
                 <div className = "linkContainer">
-                <p className = "about"><a href = "www.google.com">Terms of Service</a><span> |</span></p>
-                <p className = "tos"><a href = "www.google.com">About</a><span> |</span></p>
+                <p className = "about"><a href = "www.google.com">About</a><span> |</span></p>
+                <p className = "tos"><a href = "www.google.com">Terms of Service</a><span> |</span></p>
                 <p className = "privacy"><a href = "www.google.com">Privacy</a></p>
                 </div>
             </div>
